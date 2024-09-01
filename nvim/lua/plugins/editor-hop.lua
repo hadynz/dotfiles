1dac0017aac0f6784c89dbdb543250f1c4b485df
Hady Osman <hadyos@gmail.com>
2024-09-01 16:44:01 +1200

    Move t/f to hop functions

--- nvim/lua/plugins/editor-hop.lua
+++ nvim/lua/plugins/editor-hop.lua
@@ -1,21 +1,3 @@
-local show_which_key = function()
-  require("which-key").show("s")
-end
-
-local paste_at_char = function()
-  local hop = require("hop-yank")
-  hop.paste_char1({
-    hint_offset = -1,
-  })
-end
-
-local hop_end_of_word = function()
-  local hop = require("hop")
-  hop.hint_words({
-    hint_position = require("hop.hint").HintPosition.END,
-  })
-end
-
 return {
   -- `hop` is an EasyMotion-like plugin allowing you to jump anywhere in a
   -- document with as few keystrokes as possible.
@@ -26,16 +8,68 @@ return {
     opts = {
       multi_windows = true,
     },
-    keys = {
-      { "ss", "<cmd>HopCamelCase<cr>", desc = "Hop to word", mode = { "n", "v", "x", "o" } },
-      { "se", hop_end_of_word, desc = "Hop to word ends", mode = { "n", "v", "x", "o" } },
-      { "sl", "<cmd>HopWordCurrentLine<cr>", desc = "Hop to word on current line", mode = { "n", "v", "x", "o" } },
-      { "sp", paste_at_char, desc = "Hop paste", mode = { "n", "v", "x", "o" } },
-      { "sn", "<cmd>HopNodes<cr>", desc = "Hop to tree node", mode = { "n", "v", "x", "o" } },
-      { "s0", "<cmd>HopLine<cr>", desc = "Hop to line", mode = { "n", "v", "x", "o" } },
-      { "s_", "<cmd>HopLineStart<cr>", desc = "Hop to line start", mode = { "n", "v", "x", "o" } },
-      { "s/", "<cmd>HopPattern<cr>", desc = "Hop to pattern", mode = { "n", "v", "x", "o" } },
-      { "s", show_which_key, desc = "Show which-key" },
-    },
+    keys = function()
+      local hop = require("hop")
+      local directions = require("hop.hint").HintDirection
+
+      local keys = {
+        { "ss", "<cmd>HopCamelCase<cr>",       desc = "Hop to word",                 mode = { "n", "v", "x", "o" } },
+        { "sl", "<cmd>HopWordCurrentLine<cr>", desc = "Hop to word on current line", mode = { "n", "v", "x", "o" } },
+        { "sn", "<cmd>HopNodes<cr>",           desc = "Hop to tree node",            mode = { "n", "v", "x", "o" } },
+        { "s0", "<cmd>HopLine<cr>",            desc = "Hop to line",                 mode = { "n", "v", "x", "o" } },
+        { "s_", "<cmd>HopLineStart<cr>",       desc = "Hop to line start",           mode = { "n", "v", "x", "o" } },
+        { "s/", "<cmd>HopPattern<cr>",         desc = "Hop to pattern",              mode = { "n", "v", "x", "o" } },
+        {
+          "se",
+          function()
+            hop.hint_words({
+              hint_position = require("hop.hint").HintPosition.END
+            })
+          end,
+          desc = "Hop to word ends",
+          mode = { "n", "v", "x", "o" }
+        },
+        {
+          "f",
+          function()
+            hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+          end,
+          desc = "Hop after next char",
+          mode = { "n", "v", "x", "o" }
+        },
+        {
+          "F",
+          function()
+            hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+          end,
+          desc = "Hop after prev char",
+          mode = { "n", "v", "x", "o" }
+        },
+        {
+          "t",
+          function()
+            hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+          end,
+          desc = "Hop before next char",
+          mode = { "n", "v", "x", "o" }
+        },
+        {
+          "T",
+          function()
+            hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+          end,
+          desc = "Hop before prev char",
+          mode = { "n", "v", "x", "o" }
+        },
+        {
+          "s",
+          function()
+            require("which-key").show("s")
+          end,
+          desc = "Show which-key"
+        },
+      }
+      return keys
+    end,
   },
 }
