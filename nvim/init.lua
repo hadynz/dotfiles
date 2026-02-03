24e6bc4001bdd6d8edae4494fb1c8ebcf0225eae
Hady Osman <hadyos@gmail.com>
2026-02-03 21:15:13 +1300

    Fixing neovim symlink on linux

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,8 @@
+-- Ensure the config directory is in the runtime path when using symlinks
+local config_path = vim.fn.stdpath("config")
+if not vim.tbl_contains(vim.opt.runtimepath:get(), config_path) then
+  vim.opt.runtimepath:prepend(config_path)
+end
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
