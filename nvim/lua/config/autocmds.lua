28a8ab5595d289ac64bde120fb10afcf3f1a7f59
Hady Osman <hadyos@gmail.com>
2026-03-04 01:11:00 +1100

    Change neovim cwd on git worktree switch

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,6 +2,18 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+-- Emit OSC 7 to notify terminal emulator (e.g. wezterm) of cwd changes
+-- Why? When using git worktrees, and then create a new terminal split, we want the latter
+-- to be created with cwd of the current worktree
+vim.api.nvim_create_autocmd("DirChanged", {
+  callback = function()
+    local cwd = vim.fn.getcwd()
+    local hostname = vim.fn.hostname()
+    io.stdout:write(string.format("\027]7;file://%s%s\027\\", hostname, cwd))
+  end,
+  desc = "Emit OSC 7 on directory change for terminal cwd tracking",
+})
+
 -- Disable new line comment
 vim.api.nvim_create_autocmd("BufEnter", {
   callback = function()
