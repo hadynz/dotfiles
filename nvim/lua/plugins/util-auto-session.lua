5137f166659955b1003a75682bf036f9ceebb6b6
Hady Osman <hadyos@gmail.com>
2026-03-03 14:44:21 +1100

    Bring back auto-session reload

--- nvim/lua/plugins/util-auto-session.lua
+++ nvim/lua/plugins/util-auto-session.lua
@@ -1,7 +1,6 @@
 return {
   {
     'rmagatti/auto-session',
-    enabled = false,
     dependencies = {
       'nvim-telescope/telescope.nvim', -- Only needed if you want to use session lens
     },
