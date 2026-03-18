e79e93695c7ff63f4a0620b41cccbf18bf374a84
Hady Osman <hadyos@gmail.com>
2026-03-19 11:04:59 +1300

    Fix code diff for individual file

--- nvim/lua/plugins/editor-diff.lua
+++ nvim/lua/plugins/editor-diff.lua
@@ -6,7 +6,8 @@ return {
     keys = {
       { "<leader>gd", "<cmd>CodeDiff<cr>", desc = "Code Diff (local)" },
       { "<leader>gD", "<cmd>CodeDiff main...<cr>", desc = "Code Diff (against main)" },
-      { "<leader>gh", "<cmd>CodeDiff history<cr>", desc = "Code Diff History" },
+      { "<leader>gh", "<cmd>CodeDiff history %<cr>", desc = "Code Diff File History" },
+      { "<leader>gH", "<cmd>CodeDiff history<cr>", desc = "Code Diff Repo History" },
     },
   },
 }
