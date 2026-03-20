e0b398fe2025bed11af36dd45f1ba40e42f98681
Hady Osman <hadyos@gmail.com>
2026-03-21 11:30:57 +1300

    Added new yank remote url command

--- nvim/lua/plugins/git-gitlinker.lua
+++ nvim/lua/plugins/git-gitlinker.lua
@@ -3,8 +3,10 @@ return {
   cmd = "GitLink",
   opts = {},
   keys = {
-    { "<Leader>gr", "<cmd>GitLink! default_branch<CR>", desc = "Open Remote File (main)" },
-    { "<Leader>gR", "<cmd>GitLink!<CR>", desc = "Open Remote File" },
-    { "<Leader>gB", "<cmd>GitLink! blame<CR>", desc = "Open Remote File with Blame" },
+    { "<Leader>gr", "<cmd>GitLink! default_branch<CR>", mode = { "n", "v" }, desc = "Open Remote File (main)" },
+    { "<Leader>gR", "<cmd>GitLink!<CR>", mode = { "n", "v" }, desc = "Open Remote File" },
+    { "<Leader>gB", "<cmd>GitLink! blame<CR>", mode = { "n", "v" }, desc = "Open Remote File with Blame" },
+    { "<Leader>yr", "<cmd>GitLink default_branch<CR>", mode = { "n", "v" }, desc = "Yank Remote URL (main)" },
+    { "<Leader>yR", "<cmd>GitLink<CR>", mode = { "n", "v" }, desc = "Yank Remote URL" },
   },
 }
